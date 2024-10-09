984a60ebbefb7e14e0abffe195f12787526970f5
edwin-sie <sierraedwin94@gmail.com>
2024-10-08 22:59:11 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+#Step1
+print("The square root of 121 is", (121**0.5))
+print()
+print()
+#Step2
+print("Decimal 240 in Binary is", bin(240))
+print()
+print()
+#Step3
+print("The number 11 divides into 164:\n", 164//11, "times with a remainder of", 164%11)
+print()
+print()
+#Step4
+print("The other day in Canada it was -33 degrees in Celsius, to an American that would be", -33*9/5+32, "degrees in Fahrenheit")
+print()
+print()
+#Step5
+PI=3.14159
+RAD=9
+AREA=PI*RAD**2
+print("A circle with a radius of", RAD, "units will cover an area of", AREA, "units squared")
+print()
+print()
+#Step6
+INVEST=255000
+RATE=8
+TIME=12
+print("With an investment of $", INVEST, "at an interest rate of", RATE, "% for", TIME, "units of time.")
+print("Simple Interest would be $", INVEST*RATE*TIME/100)
+print()
+print()
+#Step7
+INVEST=350000
+RATE=7
+TIME=8
+print("With an investment of", INVEST, "at an interest rate of", RATE, "% for", TIME, "units of time.")
+print("Compound Interest would be", INVEST*(1+RATE/100)**TIME)
+print()
+print()
+#Step8
+print("Please enter an integer: 100")
+print()
